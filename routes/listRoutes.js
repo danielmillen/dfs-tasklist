564adf8640a51343daa5b5ff5fa71183a3bfa533
Daniel Millen <dan@dansclan.com>
2018-08-23 20:05:05 -0700

    Add parameters to the /lists path

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -3,7 +3,18 @@ const TaskList = mongoose.model('lists');
 
 module.exports = app => {
   app.get('/lists', async (req, res) => {
-    var lists = await TaskList.find({});
+    const searchTerms = {};
+
+    // Mongo is case sensitive, so the search will be case sensitive.
+    // There are ways around this, but for this example, I'll keep the case sensitivity
+    if (req.query.search) {
+      searchTerms.name = req.query.search;
+    }
+
+    // parseInt returns NaN if the value passed can't be parsed. limit and skip treat this as 0, so it's safe
+    var lists = await TaskList.find(searchTerms)
+      .limit(parseInt(req.query.limit))
+      .skip(parseInt(req.query.skip));
 
     res.send(lists);
   });
